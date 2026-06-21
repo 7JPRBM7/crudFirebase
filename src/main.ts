8e22428f1f9d6002e349d013b1d8180343ab2c12
João Macedo <joaoprbmacedo@gmail.com>
2026-06-21 12:33:40 -0300

    chore: configura provedores globais do firebase e http client no main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,33 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
+// ferramenta HTTP para liberar o consumo da API REST
+import { provideHttpClient } from '@angular/common/http';
+
+// ferramentas base do Firebase
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: "COLE_AQUI_DEPOIS",
+  authDomain: "COLE_AQUI_DEPOIS",
+  projectId: "COLE_AQUI_DEPOIS",
+  storageBucket: "COLE_AQUI_DEPOIS",
+  messagingSenderId: "COLE_AQUI_DEPOIS",
+  appId: "COLE_AQUI_DEPOIS"
+};
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    
+    // provedor HTTP para o aplicativo inteiro
+    provideHttpClient(),
+    
+    // inicia a conexão global com o Firebase e o Banco de Dados
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore())
   ],
 });
